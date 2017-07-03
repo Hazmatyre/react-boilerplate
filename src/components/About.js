79971fcb49ee2b80f9f7f23a5592cdc7a3ed34ea
Haziq Hamdan <haziq.hamdan@wearenova.co.uk>
2017-07-03 16:59:47 +0100

    Added more plain text to About & changed line height

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 const About = ({}) => (
   <div>
     <h1>About</h1>
-    <p>This is a pretty neat app and you should totally like it.</p>
+    <p>This is a pretty neat app and you should totally like it. It's build on react, and I have build this for some Udemy course.</p>
+    <p>Here are some of the tools I used:</p>
+    <ul>
+      <li><a href="">React</a> - This was the JS framework used.</li>
+      <li><a href="">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.</li>
+    </ul>
   </div>
 
 );
